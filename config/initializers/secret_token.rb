b7c93d72d8e3884e2489498e86ed35717f857234
Daniel Hackney <dan@haxney.org>
2011-09-21 18:33:49 -0400

    Clean Rails 3.1.0 install
    Signed-off-by: Daniel Hackney <dan@haxney.org>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railskating::Application.config.secret_token = 'cee4a1d7ef892912bdb05e6e4cc246915c1a8e4e96c5dbc88ee46c50841bcf060a437dff3f62ca5a8e2aa5b14b106e3fe13787890a376b12cf0a5545c9cc1bc2'
