fd64d5a70f81ca207d1d70d84f7313382cb4fe7b
Daniel Hackney <dan@haxney.org>
2011-09-22 23:31:16 -0400

    Create seed data for `dances`, `levels`, and `sections`.
    One weakness of this approach is that the `Dance` seeds are strongly coupled to
    the `Section` ids. Since it is unlikely that new sections will be created, this
    is probably not the end of the world.

---
+++ db/fixtures/levels.rb
@@ -0,0 +1,41 @@
+Level.seed do |l|
+  l.id     = 1
+  l.name   = "Newcomer"
+  l.weight = 100
+end
+
+Level.seed do |l|
+  l.id     = 2
+  l.name   = "Bronze"
+  l.weight = 200
+end
+
+Level.seed do |l|
+  l.id     = 3
+  l.name   = "Silver"
+  l.weight = 300
+end
+
+Level.seed do |l|
+  l.id     = 4
+  l.name   = "Gold"
+  l.weight = 300
+end
+
+Level.seed do |l|
+  l.id     = 5
+  l.name   = "Open"
+  l.weight = 400
+end
+
+Level.seed do |l|
+  l.id     = 6
+  l.name   = "Pre-Championship"
+  l.weight = 450
+end
+
+Level.seed do |l|
+  l.id     = 7
+  l.name   = "Championship"
+  l.weight = 470
+end
---
+++ db/fixtures/sections.rb
@@ -0,0 +1,19 @@
+Section.seed do |s|
+  s.id   = 1
+  s.name = "Standard"
+end
+
+Section.seed do |s|
+  s.id   = 2
+  s.name = "Latin"
+end
+
+Section.seed do |s|
+  s.id   = 3
+  s.name = "Smooth"
+end
+
+Section.seed do |s|
+  s.id   = 4
+  s.name = "Rhythm"
+end
