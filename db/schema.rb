f130cbcd7d8687a30c2e99ce31ad553bac7bb8f0
Daniel Hackney <dan@haxney.org>
2011-09-22 21:36:13 -0400

    Add `Competition` model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110923013021) do
+ActiveRecord::Schema.define(:version => 20110923013449) do
+
+  create_table "competitions", :force => true do |t|
+    t.string   "name"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "team_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
