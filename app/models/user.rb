8b931723b05afc3cb1322fb0e7409f5e1895ee16
Daniel Hackney <dan@haxney.org>
2011-09-27 17:10:50 -0400

    Initial associations for `User` model.
    They are to `Team` and `Couple` (both lead and follow) for now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  belongs_to :team
+  has_many :lead_couples, class_name: 'Couple', foreign_key: :lead_id
+  has_many :follow_couples, class_name: 'Couple', foreign_key: :follow_id
 end
