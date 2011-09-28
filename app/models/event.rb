e15a27b5eee3e6d2b5406c4f183946a269cc68b3
Daniel Hackney <dan@haxney.org>
2011-09-28 12:52:47 -0400

    Associations between `Event` and `Round`.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   belongs_to :competition
   belongs_to :level
   has_many :couples
+  has_many :rounds
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,2 +1,4 @@
 class Round < ActiveRecord::Base
+  belongs_to :event
+  has_many :sub_rounds
 end
