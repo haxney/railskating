0cae30d7ab39306f97ccc75d02fd681c4eca84b6
Daniel Hackney <dan@haxney.org>
2011-09-28 12:41:17 -0400

    Basic associations for `Couple`.
    All of its `belong_to`s and a `:through` association to `Competition`.

--- app/models/couple.rb
+++ app/models/couple.rb
@@ -1,2 +1,6 @@
 class Couple < ActiveRecord::Base
+  belongs_to :lead, class_name: 'User', inverse_of: :lead_couples
+  belongs_to :follow, class_name: 'User', inverse_of: :follow_couples
+  belongs_to :event, inverse_of: :couples
+  has_one :competition, through: :event
 end
