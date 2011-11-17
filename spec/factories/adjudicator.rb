e1d6069d8ed4e632258f68ef2a2ff7c8f77269d1
Daniel Hackney <dan@haxney.org>
2011-11-17 17:22:27 -0500

    Make `Adjudicator#shorthand` factory a sequence.
    This ensures no collisions.

--- spec/factories/adjudicator.rb
+++ spec/factories/adjudicator.rb
@@ -8,6 +8,6 @@ FactoryGirl.define do
   factory :random_adjudicator, parent: :adjudicator do
     association :user, factory: :random_user
     association :competition, factory: :random_competition
-    shorthand { Forgery(:basic).text exactly: 1, allow_lower: false }
+    sequence(:shorthand)
   end
 end
