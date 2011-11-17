adab2569f206f5a0e1d70fb0a35bb58620abcf0c
Daniel Hackney <dan@haxney.org>
2011-11-17 17:24:18 -0500

    Set up basic `round` factories.

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Round do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#recall" do
+    let(:competition) { FactoryGirl.create(:competition) }
+    let(:event) { FactoryGirl.create(:event, competition: competition) }
+    let(:sub_event) { FactoryGirl.create(:sub_event, event: event) }
+    let(:round) { FactoryGirl.create(:round, event: event, requested: 6) }
+    let(:sub_round) { FactoryGirl.create(:sub_round, round: round, sub_event: sub_event) }
+    let(:couples) { FactoryGirl.create_list(:random_couple, 10, event: event) }
+    let(:adjudicators) { FactoryGirl.create_list(:random_adjudicator, 7, competition: competition) }
+
+    context "when unambiguous" do
+
+    end
+  end
+
 end
