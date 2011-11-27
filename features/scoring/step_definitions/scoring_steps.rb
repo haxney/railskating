0b2ce31fea45a91483257aa57a50f76c5fad9613
Daniel Hackney <dan@haxney.org>
2011-11-27 17:48:25 -0500

    Fix `Given` step for marks.
    It was creating only a single `Adjudicator` and `Couple` and assigning all marks
    to them. The proper fields need to be included in order for the `Couple`s and
    `Adjudicator`s to be created when they ought to be.
    
    Signed-off-by: Daniel Hackney <dan@haxney.org>

--- features/scoring/step_definitions/scoring_steps.rb
+++ features/scoring/step_definitions/scoring_steps.rb
@@ -13,28 +13,40 @@ Given(/^the adjudicators marked the following couples (?:for|in) #{capture_model
     if header == 'couple'
       header
     else
-      find_or_create_model("adjudicator", header, competition: competition)
+      find_or_create_model("adjudicator",
+                           header,
+                           competition: competition,
+                           shorthand: header)
     end
   end
 
   # Create the couple and assign them marks for the sub round.
-  table.hashes.map do |row|
+  table.hashes.each do |row|
     label = row.delete('couple')
-    couple = find_or_create_model('couple', label, event: event)
-    row.map do |judge, place|
-      place = (place == 'X') ? 0 : place.to_i
+    couple = find_or_create_model('couple',
+                                  label,
+                                  event: event,
+                                  number: label.to_i)
+
+    # Each adjudicator's mark (or lack thereof) for the current couple
+    row.each do |judge, place|
+      place = '0' if place == 'X'
 
       # Only create the mark if the entry in the table is not ' '.
       create_model('mark',
                    adjudicator: judge,
                    couple: couple,
                    sub_round: sub_round,
-                   placement: place) unless place == ' '
+                   placement: place.to_i) if /\w+/ =~ place
     end
   end
 end
 
+# Find or create a model with the given fields.
+#
+# Since there could be multiple sub rounds within a single round, don't recreate
+# the couples or adjudicators in each table.
 def find_or_create_model(factory, label, fields)
   name = "#{factory} \"#{label}\""
-  find_model(name, fields) or create_model!(name, fields)
+  find_model(name, fields) or create_model(name, fields)
 end
