327a0c789160495b22a21fa63de9edc03f3aa52d
Daniel Hackney <dan@haxney.org>
2011-12-05 18:06:27 -0500

    Clean up RSpec and Cucumber configuration.
    Plays nicer with Spork and friends.
    
    Signed-off-by: Daniel Hackney <dan@haxney.org>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,16 @@
 require 'rubygems'
 require 'spork'
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-# Loading more in this block will cause your tests to run faster. However,
-# if you change any configuration or code from libraries loaded here, you'll
-# need to restart spork for it take effect.
 Spork.prefork do
+
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   # Seed the database with the standard values.
   SeedFu.seed
@@ -28,13 +25,11 @@ Spork.prefork do
     # config.mock_with :rr
     config.mock_with :rspec
 
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
     # instead of true.
     config.use_transactional_fixtures = true
+    DatabaseCleaner.strategy = :transaction
 
     # Allows the use of shorthand `create(:user)` instead of `FactoryGirl.create(:user)`
     config.include FactoryGirl::Syntax::Methods
