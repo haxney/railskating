99995ec5bd65e7db82817047a06b7953e298e458
Daniel Hackney <dan@haxney.org>
2013-08-27 03:53:42 -0400

    Almost-working replacement for :finder-sql

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,11 @@ class User < ActiveRecord::Base
   has_many :follow_couples, class_name: 'Couple', foreign_key: :follow_id, inverse_of: :follow
   has_many :adjudicators
 
-  has_many :couples, class_name: 'Couple', finder_sql:
-    proc { <<-EOQ
-      SELECT 'couples'.* FROM 'couples' WHERE
-        'couples'.'lead_id' = #{self.id}
-        OR 'couples'.'follow_id' = #{self.id}
-    EOQ
-  }
+  has_many :couples, -> {
+    joins('JOIN couples ON (couples.lead_id = users.id OR couples.follow_id = users.id)')
+      .select('couples.*')
+  }, class_name: 'User', foreign_key: 'id'
+
   has_many :events, class_name: 'Event', finder_sql:
     proc { <<-EOQ
       SELECT 'events'.* FROM 'events'
