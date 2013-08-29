3aa13b5d0392e8725fd1a2a9d621539cfd506ed2
Daniel Hackney <dan@haxney.org>
2013-08-29 18:57:44 -0400

    Create association `prelim_results` for `Round`

--- app/models/round.rb
+++ app/models/round.rb
@@ -6,4 +6,21 @@ class Round < ActiveRecord::Base
   has_and_belongs_to_many :couples
 
   has_many :couple_tallies, class_name: 'CoupleRoundTally'
+
+  has_many :prelim_results, class_name: 'CoupleRoundTally', finder_sql:
+    proc { <<-SQL
+          SELECT t1.num_marks,
+                 (
+                   SELECT count(t2.couple_id)
+                   FROM couple_round_tallies AS t2
+                   WHERE t2.num_marks >= t1.num_marks AND
+                         t1.round_id = #{self.id}
+                 ) AS num_couples
+            FROM couple_round_tallies AS t1
+            WHERE t1.round_id = #{self.id}
+            GROUP BY t1.num_marks
+            ORDER BY t1.num_marks DESC;
+      SQL
+  }
+
 end
