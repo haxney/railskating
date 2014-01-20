1a56268c96ffcd44f914725dbbc7cf8ecdf0d064
Daniel Hackney <dan@haxney.org>
2014-01-20 15:40:10 -0500

    Fix `Adjudicator` factory shorthand generation.
    It was not resetting the sequence, so shorthand letters were generated
    which did not correspond to those in the original event.

--- spec/factories/adjudicator.rb
+++ spec/factories/adjudicator.rb
@@ -2,8 +2,6 @@ FactoryGirl.define do
   factory :adjudicator do
     user
     competition
-    # First argument passed in is 1, so we need to start one code point before
-    # 'A', which is '@'.
-    sequence(:shorthand) { |n| ('@'..'Z').to_a[n] }
+    sequence(:shorthand, 'A')
   end
 end
