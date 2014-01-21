ebc68705968a703775efe7cda13679e1980c9944
Daniel Hackney <dan@haxney.org>
2014-01-20 20:19:15 -0500

    Update gems and fix deprecation warning.
    This also solves the issue of minitest complaining when cucumber is run.

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
@@ -20,6 +19,10 @@ module Railskating
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Silence the rails 4.0.2 deprecation warnings.
+    config.i18n.enforce_available_locales = true
+
     SeedFu.quiet = true
   end
 end
