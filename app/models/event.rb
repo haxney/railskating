76000c81d9b256294961edbfd754a6773da8a28c
Daniel Hackney <dan@haxney.org>
2014-01-22 15:12:22 -0500

    Quote `order` field of `SubEvent`
    Leaving it unquoted causes problems for SQLite.

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,9 @@ class Event < ActiveRecord::Base
   belongs_to :level
   has_many :couples, dependent: :destroy
   has_many :rounds, dependent: :destroy
-  has_many :sub_events, -> { order('sub_events.order ASC') }, dependent: :destroy
-  has_many :dances, -> { select('"dances".*, sub_events.order')
-      .distinct.order('sub_events.order ASC') }, through: :sub_events
+  has_many :sub_events, -> { order('"sub_events"."order" ASC') }, dependent: :destroy
+  has_many :dances, -> { select('"dances".*, "sub_events"."order"')
+      .distinct.order('"sub_events"."order" ASC') }, through: :sub_events
   has_many :sections, -> { distinct }, through: :dances
   has_many :placements, -> { order :rank }, dependent: :destroy
 
