d34e915a88f2a40d6d4a3bb0c180a8982b6a5c1d
Daniel Hackney <dan@haxney.org>
2014-01-23 22:16:38 -0500

    Clean up header class names.

--- app/helpers/adjudicators_helper.rb
+++ app/helpers/adjudicators_helper.rb
@@ -1,2 +1,7 @@
 module AdjudicatorsHelper
+
+  # Builds an array of classes to apply to table headers for this {Adjudicator}.
+  def adjudicator_header_classes(adjudicator)
+    ['adjudicator_header', "adjudicator_header_#{adjudicator.shorthand}"]
+  end
 end
