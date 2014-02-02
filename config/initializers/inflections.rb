dcbd6aa9fabc659db0087fd58768d82832ffbb7d
Daniel Hackney <dan@haxney.org>
2014-02-01 19:49:18 -0500

    I18n `ActiveSupport::Inflector#ordinalize`
    Uses the code from
    http://kalmbach.wordpress.com/2013/01/08/ordinalize-numbers-in-current-locale/
    and can add additional translations.

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,3 +8,35 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
+
+module ActiveSupport
+  module Inflector
+    # Returns the ordinal string corresponding to the receiver integer.
+    #
+    # Internationalized from
+    # http://kalmbach.wordpress.com/2013/01/08/ordinalize-numbers-in-current-locale/
+    #
+    # Uses English by default, but can be customized by adding
+    def ordinalize(number)
+      abs_number = number.to_i.abs
+      rules = I18n.t 'number.ordinals', :default => ""
+
+      # Assume English for compat
+      rules = {
+        :'\A\d{0,}(11|12|13)\z' => "%dth",
+        :'\A\d{0,}1\z'  => "%dst",
+        :'\A\d{0,}2\z'  => "%dnd",
+        :'\A\d{0,}3\z'  => "%drd",
+        :other => "%dth"
+      } if rules.empty?
+
+      match = rules.find do |rule|
+        Regexp.new(rule[0].to_s).match(abs_number.to_s)
+      end
+
+      match = match && match[1] || rules[:other]
+
+      match % number
+    end
+  end
+end
