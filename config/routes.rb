706121ec942b29fe3a55a8354e8216f7ff119e3f
Daniel Hackney <dan@haxney.org>
2014-02-02 23:43:36 -0500

    Remove unused controllers, views, and specs.
    The only routes which remain are "/competitions", "/competitions/:id",
    and "/competitions/:id/events/:number", so all other controllers and
    views can be deleted.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Railskating::Application.routes.draw do
   root 'competitions#index'
 
   resources :competitions, only: [:index, :show] do
-    resources :events, only: [:index]
     get 'events/:number', to: 'events#show', as: :event
   end
 end
--- spec/routing/events_routing_spec.rb
+++ spec/routing/events_routing_spec.rb
@@ -2,34 +2,10 @@ require "spec_helper"
 
 describe EventsController do
   describe "routing" do
-
-    it "routes to #index" do
-      get("/events").should route_to("events#index")
-    end
-
-    it "routes to #new" do
-      get("/events/new").should route_to("events#new")
-    end
-
     it "routes to #show" do
-      get("/events/1").should route_to("events#show", :id => "1")
+      get('/competitions/1/events/2').should route_to('events#show',
+                                                      competition_id: '1',
+                                                      number: '2')
     end
-
-    it "routes to #edit" do
-      get("/events/1/edit").should route_to("events#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      post("/events").should route_to("events#create")
-    end
-
-    it "routes to #update" do
-      put("/events/1").should route_to("events#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      delete("/events/1").should route_to("events#destroy", :id => "1")
-    end
-
   end
 end
