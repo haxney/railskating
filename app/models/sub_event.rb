f57dfc4b0003af700c6cc848a65626dd50b23faf
Daniel Hackney <dan@haxney.org>
2014-02-12 17:38:12 -0500

    Remove debug code
    Whoops! It shouldn't have been left in there.

--- app/models/sub_event.rb
+++ app/models/sub_event.rb
@@ -38,13 +38,11 @@ class SubEvent < ActiveRecord::Base
     end
 
     places = Scrutineering.place_one_dance(finalists)
-    adsf = places.map do |elt|
+    places.map do |elt|
       # Potential issues with floating point here, but without converting, there
       # was an argument error.
       self.sub_placements.create(rank: elt.first.to_f, couple_id: elt.second.id)
     end
-
-    adsf
   end
 
   # Build an array of the form returned by {#Scrutineering::compute_all_places}.
