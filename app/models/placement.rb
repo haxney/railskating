33d7d84523390bb1fc25819bc4d54de47ab671bd
Daniel Hackney <dan@haxney.org>
2014-02-12 21:22:34 -0500

    Remove unused `rank_to_s` from `Placement`
    This was from when I was still figuring out the appropriate interface
    for converting the rank.

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,10 +1,4 @@
 class Placement < ActiveRecord::Base
   belongs_to :couple
   belongs_to :event
-
-  # Returns a string representation of the rank suitable for display to the
-  # user. Present to match the signature of {SubPlacement::rank_to_s}
-  def rank_to_s
-    rank.to_s
-  end
 end
