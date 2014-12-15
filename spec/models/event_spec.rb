b263242e9b9bf0c349eed68a514a4a89a95a9a64
Daniel Hackney <dan@haxney.org>
2014-12-14 18:26:32 -0800

    Correct SubEvent selection from Event
    The old selection syntax was messing with `COUNT()` queries.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -19,5 +19,17 @@ describe Event do
       expect(SubEvent.where(event_id: event.id).count).to eq(0)
       expect { @sub_event.reload }.to raise_error
     end
+
+    it "finds SubEvents in order" do
+      se2 = create(:sub_event, event: event,
+                   dance: Constants::Dances::AMERICAN_CHA_CHA, weight: 2)
+      se1 = create(:sub_event, event: event,
+                   dance: Constants::Dances::AMERICAN_MAMBO, weight: 1)
+      se3 = create(:sub_event, event: event,
+                   dance: Constants::Dances::AMERICAN_RUMBA, weight: 3)
+      sub_events = [se1, se2, se3]
+      expect(event.sub_events.count).to eq(3)
+      expect(event.sub_events).to eq(sub_events)
+    end
   end
 end
