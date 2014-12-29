3c7155555a575067789bb021d4e22b6cd236cc03
Daniel Hackney <dan@haxney.org>
2014-12-28 20:53:46 -0500

    Use random test order.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ Railskating::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.active_support.test_order = :random # `:sorted` for old behavior
 end
